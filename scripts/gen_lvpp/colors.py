99bd6d863a4f19d29227b533148a21673253c66b
Ryan Stachura <rstachurr@gmail.com>
2026-03-29 23:52:45 -0700

    rename all s to string for readability

--- scripts/gen_lvpp/colors.py
+++ scripts/gen_lvpp/colors.py
@@ -5,57 +5,57 @@ if __name__ == "__main__":
 
 
 
-def _make_color(s: str, v: int, *, reset: bool = True):
-    s = "\x1b[{}m{}".format(v, s)
+def _make_color(string: str, v: int, *, reset: bool = True):
+    string = "\x1b[{}m{}".format(v, string)
 
     if reset:
-        s += "\x1b[39m"
+        string += "\x1b[39m"
 
-    return s
+    return string
 
 def _constrain_255(v: int):
     return min(255, max(0, v))
 
 
 
-def gray(s: str):
-    return _make_color(s, 90)
+def gray(string: str):
+    return _make_color(string, 90)
 
-def red(s: str):
-    return _make_color(s, 91)
+def red(string: str):
+    return _make_color(string, 91)
 
-def green(s: str):
-    return _make_color(s, 92)
+def green(string: str):
+    return _make_color(string, 92)
 
-def yellow(s: str):
-    return _make_color(s, 93)
+def yellow(string: str):
+    return _make_color(string, 93)
 
-def blue(s: str):
-    return _make_color(s, 94)
+def blue(string: str):
+    return _make_color(string, 94)
 
-def magenta(s: str):
-    return _make_color(s, 95)
+def magenta(string: str):
+    return _make_color(string, 95)
 
-def cyan(s: str):
-    return _make_color(s, 96)
+def cyan(string: str):
+    return _make_color(string, 96)
 
 
 
-def rgb(s: str, r: int, g: int, b: int, *, reset: bool = True):
+def rgb(string: str, r: int, g: int, b: int, *, reset: bool = True):
     r = _constrain_255(r)
     g = _constrain_255(g)
     b = _constrain_255(b)
-    return _make_color(s, "38;2;{};{};{}".format(r,g,b), reset=reset)
+    return _make_color(string, "38;2;{};{};{}".format(r,g,b), reset=reset)
 
-def hex(s: str, hexcode: int, *, reset: bool = True):
+def hex(string: str, hexcode: int, *, reset: bool = True):
     b = hexcode & 0xff
     hexcode >>= 8
     g = hexcode & 0xff
     hexcode >>= 8
     r = hexcode & 0xff
-    return rgb(s, r, g, b, reset=reset)
+    return rgb(string, r, g, b, reset=reset)
 
 
 
-def reset(s: str):
-    return s + "\x1b[0m"
+def reset(string: str):
+    return string + "\x1b[0m"
